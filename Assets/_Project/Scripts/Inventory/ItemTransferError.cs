3468a35c22651264716b6866b81e549fd67d7af6
Max <kudinov.maxim.99@gmail.com>
2026-06-06 23:45:14 +0300

    Add ItemTransferService core

---
+++ Assets/_Project/Scripts/Inventory/ItemTransferError.cs
@@ -0,0 +1,12 @@
+public enum ItemTransferError
+{
+    None,
+    InvalidSource,
+    InvalidTarget,
+    SameSlot,
+    EmptySource,
+    TargetRejectsSource,
+    SourceRejectsTarget,
+    CannotMerge,
+    CannotSwap
+}
